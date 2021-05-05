40c84fd178dc7a7ff9fd33c6692b415d906b93bc
Anette <anet1@rocketmail.com>
2021-05-05 21:53:28 +0100

    Added localStorage functionality, Rearranged and cleaned code

--- app.js
+++ app.js
@@ -3,41 +3,70 @@ const list = document.querySelector('.todos');
 const search = document.querySelector('.search input');
 const date = document.querySelector('.date');
 
-const generateTemplate = todo => {
-  const html = `
-  <li class="list-group-item d-flex justify-content-between align-items-center">
-     <span>${todo}</span>
-     <i class="far fa-trash-alt delete"></i>
-  </li>
-  `;
+let taskList;
+const generateTemplate = () => {
+  let taskListShow = list;
 
-  list.innerHTML += html;
+  let html = '';
+  let localItems = JSON.parse(localStorage.getItem('todos'));
+
+  if (localItems === null) {
+    taskList = [];
+  } else {
+    taskList = localItems;
+  }
+
+  taskList.forEach((todo, index) => {
+    html += `
+        <li class="list-group-item d-flex justify-content-between align-items-center" id=${index}>
+           <span>${todo}</span>
+           <i onClick="() => deleteTodos(${index})" class="far fa-trash-alt delete"></i>           
+       </li>
+    `;
+  });
+  taskListShow.innerHTML = html;
 };
 
-addForm.addEventListener('submit', e => {
+const addTodo = e => {
   e.preventDefault();
+  let localItems = JSON.parse(localStorage.getItem('todos'));
+
+  if (localItems === null) {
+    taskList = [];
+  } else {
+    taskList = localItems;
+  }
 
   const todo = addForm.add.value.trim();
+  taskList.push(todo);
+  console.log(taskList);
+  localStorage.setItem('todos', JSON.stringify(taskList));
 
   if (todo.length) {
     generateTemplate(todo);
     addForm.reset();
   }
-});
+};
 
-list.addEventListener('click', e => {
-  if (e.target.classList.contains('delete')) {
-    e.target.parentElement.remove();
+const deleteTodos = index => {
+  if (index.target.classList.contains('delete')) {
+    const www = Number(index.target.parentElement.id);
+    taskList.splice(www, 1);
+    localStorage.setItem('todos', JSON.stringify(taskList));
+
+    let generateListID = index.target.parentNode.id;
+    document.getElementById(generateListID).remove();
   }
+};
 
-  console.log(e.target.parentElement.tagName);
-
+const completedTodos = e => {
   if (e.target.parentElement.tagName === 'LI') {
     e.target.classList.toggle('completed');
   }
-});
+};
 
-const filterTodos = term => {
+const filterTodos = () => {
+  const term = search.value.trim().toLowerCase();
   Array.from(list.children)
     .filter(todo => !todo.textContent.toLowerCase().includes(term))
     .forEach(todo => todo.classList.add('filtered'));
@@ -47,14 +76,11 @@ const filterTodos = term => {
     .forEach(todo => todo.classList.remove('filtered'));
 };
 
-search.addEventListener('keyup', () => {
-  const term = search.value.trim().toLowerCase();
+// event listenters
+addForm.addEventListener('submit', addTodo);
+list.addEventListener('click', deleteTodos);
+list.addEventListener('click', completedTodos);
+search.addEventListener('keyup', filterTodos);
 
-  filterTodos(term);
-});
-
-const getNewDate = () => {
-  date.innerHTML = new Date().getFullYear();
-};
-
-getNewDate();
+date.textContent = new Date().getFullYear();
+generateTemplate();
